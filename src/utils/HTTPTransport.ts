0ac46534f75f2a0b3ec37b12f2cb6b27dbc87dd8
Sergey Podryabinnikov <valentine94@inbox.ru>
2022-03-01 21:52:03 +0300

    added HTTPTransport

---
+++ src/utils/HTTPTransport.ts
@@ -0,0 +1,73 @@
+enum Methods {
+  GET = 'GET',
+  PUT = 'PUT',
+  POST = 'POST',
+  DELETE = 'DELETE'
+}
+
+type MethodOptions = {
+  timeout: number;
+  data?: Record<string, unknown>;
+  headers?: Record<string, string>;
+};
+
+type RequestOptions = {
+  timeout: number;
+  data?: Record<string, unknown>;
+  headers?: Record<string, string>;
+  method: Methods;
+};
+
+export default class HTTPTransport {
+  queryStringify(data: Record<string, unknown>) {
+    const keys = Object.keys(data);
+    return keys.reduce((result, key, index) => {
+      return `${result}${key}=${data[key]}${index < keys.length - 1 ? '&' : ''}`;
+    }, '?');
+  }
+
+  get(url: string, options: MethodOptions) {
+    if (options.data) {
+      url += this.queryStringify(options.data);
+    }
+
+    return this.request(url, { ...options, method: Methods.GET }, options.timeout);
+  }
+
+  post(url: string, options: MethodOptions) {
+    return this.request(url, { ...options, method: Methods.POST }, options.timeout);
+  }
+
+  put(url: string, options: MethodOptions) {
+    return this.request(url, { ...options, method: Methods.PUT }, options.timeout);
+  }
+
+  delete(url: string, options: MethodOptions) {
+    return this.request(url, { ...options, method: Methods.DELETE }, options.timeout);
+  }
+
+  request(url: string, options: RequestOptions, timeout = 5000) {
+    const { method, data, headers = {} } = options;
+
+    return new Promise((resolve, reject) => {
+      const xhr = new XMLHttpRequest();
+      xhr.open(method, url);
+
+      Object.keys(headers).forEach((key) => {
+        xhr.setRequestHeader(key, headers[key]);
+      });
+
+      xhr.timeout = timeout;
+      xhr.onload = () => resolve(xhr);
+      xhr.onabort = reject;
+      xhr.onerror = reject;
+      xhr.ontimeout = reject;
+
+      if (method === 'GET' || !data) {
+        xhr.send();
+      } else {
+        xhr.send((data as unknown as XMLHttpRequestBodyInit));
+      }
+    });
+  }
+}
